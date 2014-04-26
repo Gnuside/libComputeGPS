5af9620a4d8576b15fc118aa290acf9733059782
Roland Laurès <roland.laures@gnuside.com>
2014-04-26 23:57:11 +0200

    the error was in the test case

--- tests/check_heading_approxHeadingFromStartToArrival.c
+++ tests/check_heading_approxHeadingFromStartToArrival.c
@@ -5,37 +5,37 @@ START_TEST (test_Heading_approxHeadingFromStartToArrival)
 	cgps_coord_t s = { lat : 48848397, lon : 2395919 },
 							 // center Place de la Nation
 							 a[] = {
-								 { lat: 48851927, lon: 2396155},
+								 { lat: 48851927, lon: 2396155}, // 0
 								 // North of Place de la Nation
-								 { lat: 48850741, lon: 2397142},
+								 { lat: 48850741, lon: 2397142}, // 1
 								 // North-North-East of Place de la Nation
-								 { lat: 48850162, lon: 2397829},
+								 { lat: 48850162, lon: 2397829}, // 2
 								 // North-East of Place de la Nation
-								 { lat: 48848785, lon: 2397338},
+								 { lat: 48848785, lon: 2397338}, // 3
 								 // North-East-East of Place de la Nation
-								 { lat: 48848407, lon: 2397462},
+								 { lat: 48848407, lon: 2397462}, // 4
 								 // East of Place de la Nation
-								 { lat: 48847863, lon: 2397821},
+								 { lat: 48847863, lon: 2397821}, // 5
 								 // South-East-East of Place de la Nation
-								 { lat: 48847560, lon: 2397102},
+								 { lat: 48847560, lon: 2397102}, // 6
 								 // South-East of Place de la Nation
-								 { lat: 48847267, lon: 2396635},
+								 { lat: 48847267, lon: 2396635}, // 7
 								 // South-South-East of Place de la Nation
-								 { lat: 48847292, lon: 2395874},
+								 { lat: 48847292, lon: 2395874}, // 8
 								 // South of Place de la Nation
-								 { lat: 48847352, lon: 2395300},
+								 { lat: 48847352, lon: 2395300}, // 9
 								 // South-South-West of Place de la Nation
-								 { lat: 48847563, lon: 2394661},
+								 { lat: 48847581, lon: 2394731}, // 10
 								 // South-West of Place de la Nation
-								 { lat: 48847969, lon: 2394366},
+								 { lat: 48847969, lon: 2394366}, // 11
 								 // South-West-West of Place de la Nation
-								 { lat: 48848425, lon: 2394297},
+								 { lat: 48848425, lon: 2394297}, // 12
 								 // West of Place de la Nation
-								 { lat: 48848778, lon: 2394339},
+								 { lat: 48848778, lon: 2394339}, // 13
 								 // North-West-West of Place de la Nation
-								 { lat: 48849155, lon: 2394618},
+								 { lat: 48849191, lon: 2395005}, // 14
 								 // North-West of Place de la Nation
-								 { lat: 48849357, lon: 2395402}
+								 { lat: 48849357, lon: 2395402}  // 15
 								 // North-North-West of Place de la Nation
 								};
 	cgps_heading_t res,
@@ -53,6 +53,7 @@ START_TEST (test_Heading_approxHeadingFromStartToArrival)
 								 	 CGPS_HEADING_SOUTH_WEST,
 								 	 CGPS_HEADING_SOUTH_WEST_WEST,
 								 	 CGPS_HEADING_WEST,
+								 	 CGPS_HEADING_NORTH_WEST_WEST,
 								 	 CGPS_HEADING_NORTH_WEST,
 								 	 CGPS_HEADING_NORTH_NORTH_WEST
 								 };
